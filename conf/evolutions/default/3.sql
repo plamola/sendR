0b3d93e593cf8237da2807314044a59286008d60
Matthijs <matthijs@plamola.com>
2014-05-24 08:46:25 +0200

    Removed unused sequence and column from account table

--- conf/evolutions/default/3.sql
+++ conf/evolutions/default/3.sql
@@ -2,9 +2,17 @@
 ALTER TABLE TRANSFORMER
 ALTER COLUMN id bigint NOT NULL DEFAULT nextval('transformer_seq');
 
+DROP SEQUENCE account_seq;
+
+ALTER TABLE account
+DROP COLUMN name;
 
 # --- !Downs
 ALTER TABLE TRANSFORMER
 ALTER COLUMN  id bigint NOT NULL;
 
+CREATE SEQUENCE account_seq;
+
+ALTER TABLE account
+ADD COLUMN name varchar(255);
 
